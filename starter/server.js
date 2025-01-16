b121d5bbedb7e848cfe762ba828011d2d167f139
DucAnh <anhnguyen2k373@gmail.com>
2025-01-16 09:03:37 +0700

    edit ex4

--- starter/server.js
+++ starter/server.js
@@ -6,19 +6,37 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     
-    let readThis = fs.readFileSync("../txt/read-this.txt", { encoding: 'utf8' });
-    console.log(readThis);
+    // let readThis = fs.readFileSync("../txt/read-this.txt", { encoding: 'utf8' });
+    // console.log(readThis);
 
-    let input = fs.readFileSync("../txt/input.txt", { encoding: 'utf8'});
-    console.log(input);
+    // let input = fs.readFileSync("../txt/input.txt", { encoding: 'utf8'});
+    // console.log(input);
 
-    let final = readThis + input;
+    // let final = readThis + input;
 
-    fs.writeFileSync("../txt/final.txt", final);
-    console.log(final);
+    // fs.writeFileSync("../txt/final.txt", final);
+    // console.log(final);
+
+    // Khởi tạo một web server gửi về cho người dùng thông tin của toàn bộ file final.txt tại localhost, cổng 3000
+    // res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    // let final = fs.readFileSync("./txt/final.txt", { encoding: 'utf8' });
+    // console.log(final);
+    // res.end(final);
+    
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    if (req.url === '/') {
+        let overview = fs.readFileSync("./templates/overview.html", { encoding: 'utf8'});
+        res.end(overview);
+    } else if (req.url === "/product") {
+        let product = fs.readFileSync("./templates/product.html", { encoding: 'utf8'});
+        res.end(product);
+    } else {
+        res.statusCode = 404;
+        res.end(`<h1>PAGE NOT FOUND</h1>`);
+    }
         
 });
 
