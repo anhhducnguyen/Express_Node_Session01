42c15689fb5a9ad3a20d0d5dac067a744d444346
DucAnh <anhnguyen2k373@gmail.com>
2025-01-16 08:58:41 +0700

    ex4

--- app.js
+++ app.js
@@ -8,17 +8,24 @@ const port = 3000;
 
 const server = createServer((req, res) => {
     res.statustCode = 200;
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    if (req.url === '/') {
-        let overview = fs.readFileSync("./templates/overview.html", { encoding: 'utf8'});
-        res.end(overview);
-    } else if (req.url === "/product") {
-        let product = fs.readFileSync("./templates/product.html", { encoding: 'utf8'});
-        res.end(product);
-    } else {
-        res.statusCode = 404;
-        res.end(`<h1>PAGE NOT FOUND</h1>`);
-    }
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    
+    // Khởi tạo một web server gửi về cho người dùng thông tin của toàn bộ file final.txt tại localhost, cổng 3000
+    let final = fs.readFileSync("./txt/final.txt", { encoding: 'utf8' });
+    console.log(final);
+    res.end(final);
+
+    // res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    // if (req.url === '/') {
+    //     let overview = fs.readFileSync("./templates/overview.html", { encoding: 'utf8'});
+    //     res.end(overview);
+    // } else if (req.url === "/product") {
+    //     let product = fs.readFileSync("./templates/product.html", { encoding: 'utf8'});
+    //     res.end(product);
+    // } else {
+    //     res.statusCode = 404;
+    //     res.end(`<h1>PAGE NOT FOUND</h1>`);
+    // }
     
 });
 
